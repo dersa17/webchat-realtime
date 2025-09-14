41f61095f6ce9ff6d0f0e12bee2255c6e9c7e70a
dersa17 <dersasembiring11@gmail.com>
2025-09-14 13:45:52 +0700

    Ready Deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import router from './route';
+import express from "express";
+import dotenv from "dotenv";
+import router from "./route";
+import path from "path";
 
 dotenv.config();
 
@@ -9,6 +10,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api", router);
+
+// Deploy sebagai SPA (Single Page Application)
+if (process.env.NODE_ENV == "production") {
+  // Path absolut ke folder build frontend
+  const frontendDistPath = path.resolve(__dirname, "../../frontend/dist");
+
+  // Serve semua file statis frontend (JS, CSS, gambar, dll)
+  app.use(express.static(frontendDistPath));
+
+  // Semua route lain diarahkan ke index.html (SPA entry point)
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);

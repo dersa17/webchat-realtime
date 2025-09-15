a028816b59588656ed2abd236a1841a1055c5693
dersa17 <dersasembiring11@gmail.com>
2025-09-15 16:13:14 +0700

    messages

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -1,8 +1,8 @@
 import { Router, Request, Response } from 'express';
 import * as authController from "./controllers/auth.controller"
+import * as messageController from "./controllers/message.controller"
 import { AuthRequest, protectRoute } from './middleware/auth.middleware';
 import {arcjetProtection} from "./middleware/arcjet.middleware"
-
 
 const router = Router();
 
@@ -17,9 +17,11 @@ router.post("/auth/login", authController.login)
 router.post("/auth/logout", authController.logout)
 router.put("/auth/update-profile", protectRoute, authController.updateProfile)
 
+router.get("/messages/contacts", protectRoute, messageController.getAllContacts);
+router.get("/messages/chats", protectRoute, messageController.getChatPartners);
+router.get("/messages/:id", protectRoute, messageController.getMessagesByUserId);
+router.post("/messages/send/:id", protectRoute, messageController.sendMessage);
+
 router.get("/check", protectRoute, (req: AuthRequest, res: Response) => res.status(200).json(req.user));
-
-
-
 
 export default router;

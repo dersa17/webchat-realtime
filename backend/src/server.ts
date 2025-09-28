0290400642159d165015f7de7e44f1fab54162d1
dersa17 <dersasembiring11@gmail.com>
2025-09-28 23:20:48 +0700

    resolve __dirname issue in ESM with import.meta.url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,24 +2,26 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import router from "./route.js";
 import path from "path";
+import { fileURLToPath } from "url"; 
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
-
-
-
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json({limit: "5mb"}))
-app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
-app.use(cookieParser())
+// definisikan __dirname secara manual untuk ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json({ limit: "5mb" }));
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
 app.use("/api", router);
 
 // Deploy sebagai SPA (Single Page Application)
-if (ENV.NODE_ENV == "production") {
+if (ENV.NODE_ENV === "production") {
   // Path absolut ke folder build frontend
   const frontendDistPath = path.resolve(__dirname, "../../frontend/dist");
 
@@ -34,6 +36,5 @@ if (ENV.NODE_ENV == "production") {
 
 server.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
-  connectDB()
-
+  connectDB();
 });
